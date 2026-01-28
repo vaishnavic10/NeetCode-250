a45d3162adda58e8e9f913a17989a94768e2223c
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-01-28 22:52:52 +0530

    Create LongestSubstringWRC.java

---
+++ SlidingWindow/LongestSubstringWRC.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set <Character> set = new HashSet<>();
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right<s.length(); right++) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+        }
+
+        set.add(c);
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+}
+}
