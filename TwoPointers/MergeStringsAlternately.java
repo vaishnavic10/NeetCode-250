d5ec09822bdb6730a9c8bc745f86c9e01afa5a96
Vaishnavi Chopade <vaishnavichopade316@gmail.com>
2026-01-28 22:48:50 +0530

    Add files via upload

---
+++ TwoPointers/MergeStringsAlternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i++));
+            result.append(word2.charAt(j++));
+        }
+
+        while (i < word1.length()) {
+            result.append(word1.charAt(i++));
+        }
+
+        while (j < word2.length()) {
+            result.append(word2.charAt(j++));
+        }
+
+        return result.toString();
+    }
+}
